5bd477b5885467008811f252315ac9c58da85247
Benjamin Kobjolke <b.kobjolke@xida.de>
2025-10-25 10:43:07 +0200

    IMPROVE: title of cmd windows

--- taskscheduler_run.bat
+++ taskscheduler_run.bat
@@ -1,4 +1,5 @@
 @echo off
+title taskscheduler - run
 cd /d "%~dp0"
 call venv\Scripts\activate.bat
 python main.py %* --log-level INFO --detailed-logs false
--- taskscheduler_run_with_id.bat
+++ taskscheduler_run_with_id.bat
@@ -1,4 +1,5 @@
 @echo off
+title taskscheduler - run with id %1
 cd /d "%~dp0"
 call venv\Scripts\activate.bat
 python main.py --run_id %1
